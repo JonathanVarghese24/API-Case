09d04894dfc159ec475ed74d94c11024405be9e1
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-03-13 09:21:34 -0500

    Initial Commit

---
+++ API Case/API_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CaseApp.swift
+//  API Case
+//
+//  Created by Jonathan Varghese on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
