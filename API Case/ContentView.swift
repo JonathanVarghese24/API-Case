a14ad065c72b8edecd019eeccbb6b05bddd5c98b
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-03-15 09:31:38 -0500

    Working v1

--- API Case/ContentView.swift
+++ API Case/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var facts = [String]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(facts, id: \.self) { fact in
+                Text(fact)
+            }
+            .navigationTitle("Random Cat Facts")
+            .toolbar {
+                Button(action: {
+                    Task {
+                        await loadData()
+                    }
+                    
+                }, label: {
+                    Image(systemName: "arrow.clockwise")
+                })
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+        .alert(isPresented: $showingAlert) {
+             Alert(title: Text("Loading Error"),
+             message: Text("There was a problem loading the API Categories"),
+                   dismissButton: .default(Text("OK")))
+         }
+     }
+    
+    func loadData() async {
+        if let url = URL(string: "https://meowfacts.herokuapp.com/?count=20") {
+            if let (data,_) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(Facts.self, from: data) {
+                    facts = decodedResponse.facts
+                    return
+                }
+            }
+        }
+        showingAlert = true
     }
 }
-
 #Preview {
     ContentView()
+}
+
+struct Facts: Identifiable, Codable {
+    var id = UUID()
+    var facts: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case facts = "data"
+    }
 }
